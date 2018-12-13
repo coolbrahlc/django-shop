775c510310700ed7c873814fffcaab5307de633a
unknown <coolbrahlc>
2018-12-13 22:25:32 +0200

    'december'

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from products.models import Product
 
 
@@ -39,6 +40,9 @@ class Order(models.Model):
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
 
+    def save(self, *args, **kwargs):
+        super(Order, self).save(*args, **kwargs)
+
 
 class ProductInOrder(models.Model):
     price_pre_item = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
@@ -47,7 +51,8 @@ class ProductInOrder(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True, default=None)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, default=None)
     number = models.IntegerField(default=1)
-    is_active = models.ForeignKey(Status, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    # is_active = models.ForeignKey(Status, on_delete=models.CASCADE)
     created = models.DateField(auto_now_add=True, auto_now=False)
     updated = models.DateField(auto_now_add=False, auto_now=True)
 
@@ -55,5 +60,40 @@ class ProductInOrder(models.Model):
         return 'Продукт в корзине: {}'.format(self.product.id,)  # В каком виде возвращает данные модели
 
     class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
+
+    def save(self, *args, **kwargs):                    # !!!!!!!!!!
+        price_per_item_temp = self.product.price        # Указываем на цену товара из Product
+        self.price_pre_item = price_per_item_temp
+        self.total_price = self.number * price_per_item_temp
+        '''
+        tmp_order = self.order
+        all_products_in_oder = ProductInOrder.objects.filter(order=tmp_order, is_active=True)   # !!!!!!!!!!
+
+        order_total_price = 0
+
+        for item in all_products_in_oder:
+            order_total_price += item.total_price
+        self.order.total_amount = order_total_price
+
+        self.order.save(force_update=True)
+        '''
+        super(ProductInOrder, self).save(*args, **kwargs)
+
+
+def product_in_order_post_save(sender, instance, created, **kwargs):
+
+    tmp_order = instance.order          # instance вмсето self
+    all_products_in_oder = ProductInOrder.objects.filter(order=tmp_order, is_active=True)  # !!!!!!!!!!
+
+    order_total_price = 0
+
+    for item in all_products_in_oder:
+        order_total_price += item.total_price
+
+    instance.order.total_amount = order_total_price
+    instance.order.save(force_update=True)
+
+
+post_save.connect(product_in_order_post_save, sender=ProductInOrder)
--- products/models.py
+++ products/models.py
@@ -3,12 +3,13 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=64, blank=True, null=True, default=None)  # !!!!!!!!!!
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     description = models.TextField(blank=True, null=True, default=None)  # Несколько строк
     created = models.DateField(auto_now_add=True, auto_now=False)
     updated = models.DateField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return 'Товар {}  создан:{}'.format(self.name, self.created)  # В каком виде возвращает данные модели
+        return 'Товар {}  цена:{}'.format(self.price, self.name)  # В каком виде возвращает данные модели
 
     class Meta:
         verbose_name = 'Товар'
